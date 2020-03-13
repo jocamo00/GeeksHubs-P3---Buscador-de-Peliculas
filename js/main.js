f83c0df43769f15037a1422b0483a7453156eb2b
Jose <jose.carreres.81@gmail.com>
2020-03-13 12:35:11 +0100

    Función mostrar trailer

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ function buscar() {
                 <p>${pelicula[i].original_title}</p>
                 <p>${pelicula[i].popularity}</p>
                 <p>${pelicula[i].overview}</p>
+                <button id="btn${[i]}" onclick="trailer()">Trailer</button>
             </div>`; 
             
         } 
@@ -25,4 +26,28 @@ function buscar() {
     })  
 } 
 
+
+
+function trailer(){
+
+    var div1 = document.getElementById("idValor1");
+    var value = div1.getAttribute("value");
+    //let a = document.getElementsById("idValor0");//et b = a.getAttribute("value");
+    //let c =  document.getElementById(b);
+    //var idv = c.getAttribute("value");
+  console.log(value);
+  
+  
+  fetch(`https://api.themoviedb.org/3/movie/${value}/videos?api_key=7f8494efa25bfa40d25abdd2fd435c69`)
+             .then(res => res.json())
+              .then(data => {
+  
+                  const popular = data.results;
+  
+                 document.getElementById("trailer").innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/${popular[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;  
+  
+                  console.log(popular);
+              }) 
+  
+  }
 
