8e7730cf507c8ea6f7d38ee5a1f3c3ed0fb72bcc
Jose <jose.carreres.81@gmail.com>
2020-03-17 01:15:25 +0100

    Maquetación final index.html

--- js/main.js
+++ js/main.js
@@ -20,13 +20,13 @@ function buscar(){
                 `<div class="col-md-12 col-lg-6 col-xl-6">
                     <div class="contenido-card">
                         <div class="img-card col-sm-12 col-md-5 col-lg-5">
-                            <img class="d-block w-100 img-card" src="${rutaImg}${pelicula[i].poster_path}"></img>
+                            <img value="${pelicula[i].id}" id="btn-trailer${[i]}" class="d-block w-100 img-card" onclick="trailer()" data-toggle="modal" data-target="#myModal" src="${rutaImg}${pelicula[i].poster_path}"></img>
                         </div>
                         <div class="info-card col-sm-12 col-md-7 col-lg-7">
                             <p class="card-title title-card">${pelicula[i].original_title}</p>
                             <p class="card-text description-card">${pelicula[i].overview}</p>
-                            <p class="card-text">${pelicula[i].genre_ids}</p>
-                            <button id="btn-trailer" class="btn-trailer" onclick="trailer()">Trailer</button>
+                            <p class="card-text genero-card">${pelicula[i].genre_ids}</p>
+                            
                         </div>
                     </div>
                 </div>`;  
@@ -35,7 +35,7 @@ function buscar(){
         /* for(let j = 0; genres.length; j++){
             console.log(genres[j].id);
         }  */
-    
+    /* <button value="${pelicula[i].id}" id="btn-trailer${[i]}" class="btn-trailer" onclick="trailer()" data-toggle="modal" data-target="#myModal">Trailer</button> */
         console.log(inputSearch);
         console.log(pelicula);
     });
@@ -50,24 +50,26 @@ function buscar(){
 
 /********* MOSTRAR TRAILER *************/
 function trailer(){
+    //let p0 = document.getElementById()
 
-    var div1 = document.getElementById('btn-trailer');
-    /* console.log(div1);
-    var value = div1.getAttribute("value");  */
+   
+    let div1 = document.getElementById('btn-trailer0');
+    let value = div1.getAttribute("value");
+    console.log("hola"+value);
     //let a = document.getElementsById("idValor0");//et b = a.getAttribute("value");
     //let c =  document.getElementById(b);
     //var idv = c.getAttribute("value");
    // console.log(value);
   
-    fetch(`https://api.themoviedb.org/3/movie/454626/videos?api_key=7f8494efa25bfa40d25abdd2fd435c69`)
+    fetch(`https://api.themoviedb.org/3/movie/${value}/videos?api_key=7f8494efa25bfa40d25abdd2fd435c69`)
         .then(res => res.json())    
         .then(data => {
   
-            const popular = data.results;
+            const trailer = data.results;
   
-            document.getElementById("trailer").innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/${popular[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;  
-  
-            console.log(popular);
+            document.getElementById("trailer").innerHTML += `<iframe width="560" height="315" src="https://www.youtube.com/embed/${trailer[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+       
+            console.log(trailer);
     });  
 }  
 /********* FIN MOSTRAR TRAILER *************/
@@ -75,10 +77,9 @@ function trailer(){
 
 
 /********* MOSTRAR PELICULAS POPULARES *************/
-/* let rutaImgPopular = "https://image.tmdb.org/t/p/w300_and_h450_bestv2/";
-
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7f8494efa25bfa40d25abdd2fd435c69&language=en-US&page=1`)
-.then(res => res.json())
+/* let rutaImgPopular = "https://image.tmdb.div class="col-6 al-contenedor">
+            <input id="input-search" class="input-search" type="text" placeholder="Search for a movie"> 
+          </div>
 .then(data => {
 
     const popular = data.results;
